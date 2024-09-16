7c513e2d62a2033c2603d106eea671a83837067b
Ritam Ghosh <ritamghosh548@icloud.com>
2024-09-16 10:33:09 +0530

    add this calculator

---
+++ calculator.py
@@ -0,0 +1,16 @@
+n = 13 
+m = 5
+ans1 = n + m
+print("The sum of",n,"and",m,"is",ans1 )
+ans2 = n - m
+print("The Difference of",n,"and",m,"is",ans2 )
+ans3 = n * m
+print("The Product of",n,"and",m,"is",ans3 )
+ans4 = n / m
+print("The Quotient of",n,"and",m,"is",ans4 )
+ans5 = n % m
+print("The Remainder of",n,"and",m,"is",ans5 )
+ans6 = n ** m
+print(n," to the power",m,"is",ans6 )
+ans7 = n // m
+print("The value of floorfunction of",n,"and",m,"is",ans7 )
